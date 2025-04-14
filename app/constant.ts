3e73819b3107e17cad3a927e3353a04c26411130
xiaozhi06 <167049348+xiaozhi06@users.noreply.github.com>
2025-04-14 21:28:39 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -525,6 +525,7 @@ const googleModels = [
   "gemini-2.0-flash-thinking-exp-01-21",
   "gemini-2.0-pro-exp",
   "gemini-2.0-pro-exp-02-05",
+  "gemini-2.5-pro-exp-03-25",
 ];
 
 const anthropicModels = [
